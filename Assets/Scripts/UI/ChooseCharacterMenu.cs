c72c58e614fecbeae44e03f539240f1dda9fd53d
arco03 <dmglux13@gmail.com>
2024-04-15 12:35:51 -0500

    last changes, menu choose character

---
+++ Assets/Scripts/UI/ChooseCharacterMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ChooseCharacterMenu : MonoBehaviour
+    {
+        private int _index;
+        private GameManager _gameManager;
+        [SerializeField] private Image image;
+        [SerializeField] private TextMeshProUGUI name;
+
+        private void Start()
+        {
+            _gameManager = GameManager.Instance;
+            _index = PlayerPrefs.GetInt("PlayerIndex");
+            if (_index > _gameManager.characters.Count - 1) _index = 0;
+            ChangeScreen();
+        }
+
+        private void ChangeScreen()
+        {
+            PlayerPrefs.SetInt("PlayerIndex", _index);
+            image.sprite = _gameManager.characters[_index].characterSprite;
+            name.text = _gameManager.characters[_index].name;
+        }
+
+        public void AfterCharacter()
+        {
+            if (_index == _gameManager.characters.Count - 1) _index += 1;
+            else _index += 1;
+            ChangeScreen();
+        }
+        public void BeforeCharacter()
+        {
+            if (_index == 0) _index = _gameManager.characters.Count - 1;
+            else _index -= 1;
+            ChangeScreen();
+        }
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GameMannager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Characters;
+using UnityEngine;
+
+namespace UI
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+        public List<Character> characters;
+
+        private void Awake()
+        {
+            if (GameManager.Instance == null)
+            {
+                GameManager.Instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
