bcfff0ea4232e6bf904ca852cfc5ae7f3922f12b
arco03 <dmglux13@gmail.com>
2024-04-17 18:19:29 -0500

    Update: Finished MVC Choose Player

---
+++ Assets/Scripts/Configurations/PlayerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Characters;
+using UnityEngine;
+
+namespace Configurations
+{
+    [CreateAssetMenu(fileName = "Configuration/Player", menuName = "PlayerConfiguration")]
+    public class PlayerConfiguration : ScriptableObject
+    {
+        public List<Character> characters;
+        public Dictionary<int, Character> playerSelections = new Dictionary<int, Character>();
+    }
+}
