89c6f8c366cf0de858857a04cfadcc87c8345b6b
Ch0coflan <153124045+Ch0coflan@users.noreply.github.com>
2024-04-19 15:16:53 -0500

    19/04/2024 15:16

--- Assets/Scripts/Playable/CharacterAnimator.cs
+++ Assets/Scripts/Playable/CharacterAnimator.cs
@@ -5,10 +5,39 @@ namespace Playable
     public class CharacterAnimator : MonoBehaviour
     {
         private Animator _animator;
-
         private void Awake()
         {
             _animator = GetComponent<Animator>();
         }
+
+        public void ResetAnimation()
+        {
+            _animator.SetBool("isWalking", false);
+        }
+
+        public void Idle()
+        {
+            ResetAnimation();
+            _animator.SetTrigger("idle");
+        }
+
+        public void Attack1()
+        {
+            ResetAnimation();
+            _animator.SetTrigger("attack1");
+        }
+
+        public void Attack2()
+        {
+            ResetAnimation();
+            _animator.SetTrigger("attack2");
+        }
+
+        public void Walk()
+        {
+            ResetAnimation();
+            _animator.SetBool("isWalking", true);
+        }
+
     }
 }
--- Assets/Scripts/Playable/Player.cs
+++ Assets/Scripts/Playable/Player.cs
@@ -25,8 +25,10 @@ namespace Playable
         [SerializeField] private Transform spawnPosition;
         
         [FormerlySerializedAs("_character")] [SerializeField] private CharacterController characterController;
+        [SerializeField] private CharacterAnimator characterAnimator;
         
         private float _x;
+
         private void Start()
         {
             characterController.SetUp(hp,jumpForce, speed, attackRange, attackPointA, attackPointB, bulletPrefab, spawnPosition);
@@ -35,26 +37,41 @@ namespace Playable
         private void FixedUpdate()
         {
             characterController.Move(_x);
+            characterAnimator.Walk();
         }
         
         private void Update()
         {
             _x = Input.GetAxisRaw(horizontal);
-            if  (Input.GetKeyDown(jumpKey))
+            PJump();
+            PAttackGun();
+            PAttackMelee();
+        }
+
+        public void PJump ()
+        {
+            if (Input.GetKeyDown(jumpKey))
             {
                 characterController.Jump();
             }
-            
+        }
+
+        public void PAttackGun()
+        {
             if (Input.GetKeyDown(attackGunKey))
             {
                 characterController.AttackGun();
+                characterAnimator.Attack2();
             }
-            
+        }
+
+        public void PAttackMelee()
+        {
             if (Input.GetKeyDown(attackMeleeKey))
             {
                 characterController.AttackMelee();
+                characterAnimator.Attack2();
             }
-            
         }
     }
 }
